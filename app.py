3734f58d07a23b88e813511524bd8cac6c8f88ed
AshishS1709 <ashishs2360@gmail.com>
2025-06-17 13:18:22 +0530

    modified app.py file

--- app.py
+++ app.py
@@ -574,4 +574,4 @@ async def root():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=18081, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=18081, log_level="info")
